e1b2f598590cf01d7d01ea27d653ff7abbbcedbd
pjhellstrom <pjh.hellstrom@gmail.com>
2019-09-12 11:56:19 -0400

    Added manual teams and shifts

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   db:
     process.env.MONGODB_URI ||
-    "mongodb://dreamshiftmaster:password123@ds127968.mlab.com:27968/heroku_d9qr140d"
+    "mongodb://garibaldi:password123@ds019688.mlab.com:19688/heroku_390xcx79"
 };
